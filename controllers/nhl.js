76d3cc31db9f0fb2a02897216cadb7b93fd9095c
brandonhall96 <hall.bran96@gmail.com>
2021-05-18 00:20:58 -0400

    work on routes still some bugs

--- controllers/nhl.js
+++ controllers/nhl.js
@@ -2,22 +2,52 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/ppConfig');
 const db = require('../models');
-const axios = require('axios')
+const axios = require('axios');
+
+
+
+
+
+router.get('/', function(req, res) {
+    let teamsUrl = 'https://statsapi.web.nhl.com/api/v1/teams/';
+  
+    axios.get(teamsUrl).then(response => {
+      let teams = response.data.results
+      res.render('nhl/index', {teams: teams});
+    });
+
+});
+
 
 
 router.get('/create', (req, res) => {
     res.render('nhl/create.ejs');
 });
 
-router.get('/index', function(req, res) {
-    let teamsUrl = 'https://statsapi.web.nhl.com/api/v1/teams/';
-  
-    axios.get(teamsUrl).then(response => {
-      let teams = response.data.results
-      res.render('/nhl/index.ejs', {teams: teams.slice(0, 31)});
-    });
 
-});
+
+
+
+
+
+
+
+// router.get('/:id', (req, res) => {
+//     let teamId = req.params.teamId
+//     db.teams.findOne({where: {
+//         id: teamId
+//     }})
+
+//     .then(foundTeam => {
+//         let teamSearch = foundTeam.dataValues.name;
+//         axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${teamSearch}`)
+//         .then(response => {
+//             let teamInfo = response.data
+//             let name = teamInfo.name
+//             let arena = teamInfo.venue.name
+//         }
+//     })
+// });
 
 
 
