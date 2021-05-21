03ca4f50025713f1d69178949bf02159c7cbd3f2
brandonhall96 <hall.bran96@gmail.com>
2021-05-21 15:08:28 -0400

    tested some css

--- models/player.js
+++ models/player.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       models.player.belongsTo(models.team);
+      models.player.belongsTo(models.user);
     }
   };
   player.init({
