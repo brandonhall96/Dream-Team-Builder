57e24c742a7e07bb1bbcf14561405f82bac42e01
brandonhall96 <hall.bran96@gmail.com>
2021-05-21 18:03:36 -0400

    create player working, had to undo associations, started on team

--- migrations/20210517214111-create-team.js
+++ migrations/20210517214111-create-team.js
@@ -23,9 +23,6 @@ module.exports = {
       founded: {
         type: Sequelize.INTEGER
       },
-      userId: {
-        type: Sequelize.INTEGER
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
